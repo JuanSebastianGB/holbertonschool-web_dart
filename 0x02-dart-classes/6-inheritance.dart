305a2ad36480972fd87fbf60b60061ee2a50a4d8
JuanGonzaGB <3393@holbertonstudents.com>
2022-12-02 12:39:49 +0000

    Complete inherit class task

---
+++ 0x02-dart-classes/6-inheritance.dart
@@ -0,0 +1,34 @@
+import './6-password.dart';
+
+class User extends Password {
+  int id;
+  String name;
+  int age;
+  double height;
+  String user_password;
+
+  User(
+      {required this.name,
+      required this.age,
+      required this.height,
+      required this.id,
+      required this.user_password})
+      : super(password: user_password);
+
+  Map toJson() => {
+        'id': this.id,
+        'name': this.name,
+        'age': this.age,
+        'height': this.height
+      };
+
+  static User fromJson(Map<dynamic, dynamic> userJson) => User(
+      id: userJson["id"],
+      name: userJson["name"],
+      age: userJson["age"],
+      height: userJson["height"],
+      user_password: userJson["user_password"]);
+
+  String toString() =>
+      'User(id : $id ,name: $name, age: $age, height: $height, Password: ${this.isValid()})';
+}
