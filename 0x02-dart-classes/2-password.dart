755f1c4fee819f5d966934e8d1e67cee34ae2f32
JuanGonzaGB <3393@holbertonstudents.com>
2022-12-02 02:53:07 +0000

    2. Password

---
+++ 0x02-dart-classes/2-password.dart
@@ -0,0 +1,16 @@
+class Password {
+  String password = '';
+
+  bool isValid() {
+    return this.password.length >= 8 &&
+        this.password.length <= 16 &&
+        this.password.contains(RegExp(r'[A-Z]')) &&
+        this.password.contains(RegExp(r'[a-z]')) &&
+        this.password.contains(RegExp(r'[0-9]'));
+  }
+
+  @override
+  String toString() {
+    return 'Your Password is: ${this.password}';
+  }
+}
