fcc803000576a31aac378ada062becb0c0904863
JuanGonzaGB <3393@holbertonstudents.com>
2022-12-02 02:57:45 +0000

    3. Private Password

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,20 @@
+class Password {
+  String password = '';
+
+  Password({password}) {
+    this.password = password;
+  }
+
+  bool isValid() {
+    return this.password.length >= 8 &&
+        this.password.length <= 16 &&
+        this.password.contains(RegExp(r'[A-Z]')) &&
+        this.password.contains(RegExp(r'[a-z]')) &&
+        this.password.contains(RegExp(r'[0-9]'));
+  }
+
+  @override
+  String toString() {
+    return 'Your Password is: ${this.password}';
+  }
+}
