176ec193b614ca2492c65b3f4fcee2dc7de42ca8
Juan Sebastian Gonzalez <3393@holbertonstudents.com>
2022-12-05 21:55:24 -0500

    6. Generate Quote

---
+++ 0x03-asynchronous/Quotes/quotes.dart
@@ -0,0 +1,14 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as Http;
+
+Future<String> generateQuote(id) async {
+  String url = 'https://breakingbadapi.com/api/quotes/$id';
+  try {
+    var response = await Http.get(Uri.parse(url));
+    var quote = json.decode(response.body)[0];
+    return '${quote['author']}; ${quote['quote']}';
+  } catch (e) {
+    print('There are no quotes');
+  }
+}
